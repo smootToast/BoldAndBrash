fc1e58c06f28e08cba4ab1b2f4b7a1a9a842311f
thewa <thewa@192.168.1.212>
2021-10-27 19:34:55 -0700

    Just testing things

--- BoldAndBrash/src/boldandbrash/BoldAndBrash.java
+++ BoldAndBrash/src/boldandbrash/BoldAndBrash.java
@@ -9,10 +9,9 @@ public class BoldAndBrash {
 
     
     public static void main(String[] args) {
-        System.out.println("This is just a test");
         Painting temp = new Painting(24);
         System.out.println(temp.getDimension());
-        System.out.println("What happens if this fails?");
+
         
     }
     
